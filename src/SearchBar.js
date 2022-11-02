cb51905df2aef29738854d9f5a5e31f5ca27e20a
Adi-Goll <adigollamudi@gmail.com>
2022-11-02 13:02:30 -0400

    search bar in center of page

---
+++ src/SearchBar.js
@@ -0,0 +1,17 @@
+import './SearchBar.css';
+
+
+function SearchBar() {
+    return (
+        <div>
+            <div id="input-div">
+                <input id="search-bar" />
+            </div>
+        </div>
+
+    );
+
+}
+
+
+export default SearchBar;
